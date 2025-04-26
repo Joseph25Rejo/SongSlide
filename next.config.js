50bf30b87f582693d522bca198eb3e1cda400d2d
Joseph25Rejo <joseph25rejo@gmail.com>
2025-04-26 22:41:21 +0530

    Netlify hosting

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // Add this to suppress hydration warnings
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 2,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 }
 
